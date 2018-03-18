6b4cca563f88ffab6614668a7b109a14729b851b
Eric Griffith <EGriffith92@gmail.com>
2018-03-17 21:20:52 -0400

    Add systemd timer and unit file. Need to test them still. ElasticDNS script should be mostly done aside from a few corner-cases. Functioned perfectly fine in simple tests.

--- elasticdns.py
+++ elasticdns.py
@@ -1,62 +1,118 @@
-#! /usr/bin/which python3
+#! /usr/bin/env python3
 
 import boto3
-import socket
 import configparser
 import argparse
 import sys
 import requests
 import ipaddress
+import datetime
 
 def main(argv):
+    arglist = buildArgParser(argv)
+    
+    ipAddr = str(getIP())
+    readLastKnownIP(arglist.ipLogFilePath, ipAddr)
+    
+    configDict = parseConfig(arglist.configFilePath)
+    updateRecord(configDict, ipAddr)
+
+
+    logCurrentIP(arglist.ipLogFilePath,ipAddr)
+
+def buildArgParser(argv):
     parser = argparse.ArgumentParser(description="Update a Route53 DNS record based upon current public IP.")
     parser.add_argument('--config', '-c', dest="configFilePath", default="elasticdns.conf", help="Path to the configuration file for each run.")
-    parser.add_argument("--log-file", "-l", dest="logFilePath", default="/var/log/elasticdns.log", help="Path to the log file")
-    arglist = parser.parse_args("".split())
-    
-    global logPath 
-    logPath = arglist.logFilePath
+    parser.add_argument("--iplog", "-i", dest="ipLogFilePath", default="elasticdns.ip", help="Path to where the previous ip should be stored at.")
 
-    readConfig(arglist.configFilePath)
-    ipAddr = getIP()
-    #updateRecord(ipAddr)
+    return parser.parse_args()
 
-def readConfig(configFilePath):
+def parseConfig(configFilePath):
     config = configparser.ConfigParser()
     config.read(configFilePath)
-    #for section in config.keys():
-    #    print(section)
-    #print(logPath)
-    #print(config['DEFAULT']['AccessKey'])
-    #print(config['DEFAULT']['SecretKey'])
-    #print(config['DEFAULT']['RecordID'])
+
+    recordHeader = config.sections()[0]
+
+    configDict = {"HostedZoneId": config[recordHeader]["HostedZoneId"],
+                  "RecordSet": config[recordHeader]["RecordSet"],
+                  "TTL": int(config[recordHeader]["TTL"]),
+                  "Type": config[recordHeader]["Type"],
+                  "Comment": config[recordHeader]["Comment"]          
+                }
+    
+    return configDict
+
+def logCurrentIP(ipLogFilePath, ipAddr):
+    with open(ipLogFilePath, "w") as ipLog:
+        ipLog.write(ipAddr)
+
+def readLastKnownIP(ipLogFilePath, newIP):
+    try:
+        with open(ipLogFilePath, 'r') as ipLog:
+            oldIP = ipLog.read()
+    except FileNotFoundError:
+        oldIP = ""
+
+    if oldIP == newIP:
+        print("The last IP that was logged matches our current public IP. Assuming records are up to date. Exitting.")
+        sys.exit(0)
+    else:
+        print("New IP: ", newIP, " does not match previous IP: ", oldIP, ", Updating records.")
+        return 0
 
 def getIP():
-    response = requests.get('https://checkip.amazonaws.com')
-    print("getIP received:", response.text)
-    response = response.strip("\n")
-    validateIP(response.text)
-    #if validateIP(str(response.text)) == True:
-    #    return response
-    #else:
-    #    print("ERROR: Did not get back a valid IP. Response was:", response.text)
-    #    sys.exit(1)
+    try:
+        response = requests.get('https://checkip.amazonaws.com')
+    except ConnectionError:
+            print("Connection to https://checkip.amazonaws.com failed.")
+            sys.exit(1)
+    except TimeoutError:
+            print("Connection to https://checkip.amazonaws.com timed out.")
+            sys.exit(1)
+
+    response = response.text.strip("\n")
+
+    if validateIP(response) == True:
+        print("Our current IP is: ", response)
+        return response
+    else:
+        sys.exit(1)
 
 def validateIP(address):
-    address = (str(address).strip("\n"))
-    print("validateIP received:", address)
-    #try:
-    print(ipaddress.ip_address(address))
-    #except ValueError:
-    #    print('address is invalid for IPv4:', address)
-    #    return False
+    try:
+        ipaddress.ip_address(address)
+    except ValueError:
+        print("Response does not pass validation: ", address)
+        return False
 
-    return 0
+    return True
 
-def updateRecord():
-    return 0
+def updateRecord(configDict, ipAddr):
+    if configDict['Comment'] == "":
+        configDict['Comment'] = "Updating record at: " + str(datetime.datetime.now())
 
-def log(logMSG):
+    client = boto3.client("route53")
+    response = client.change_resource_record_sets(
+    HostedZoneId=configDict['HostedZoneId'],
+    ChangeBatch={
+        'Comment': configDict['Comment'],
+        'Changes': [
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': configDict['RecordSet'],
+                    'Type': configDict['Type'],
+                    'TTL': configDict['TTL'],
+                    'ResourceRecords': [
+                        {
+                            'Value': ipAddr
+                        },
+                    ],
+                }
+            },
+            ]
+        }
+    )
     return 0
 
 if __name__ == "__main__":
